3a8b7acde96179dcb845542022a29df2480f9916
Ivan Maksimovic <ivanandresm@gmail.com>
2021-05-26 17:16:38 -0400

    got raylib

---
+++ Apex Answers/Apex Answers/Apex_AnswersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_AnswersApp.swift
+//  Apex Answers
+//
+//  Created by Ivan Maksimovic on 26/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Apex_AnswersApp: App {
+  var body: some Scene {
+    WindowGroup {
+        ContentView()
+    }
+  }
+}
---
+++ Apex Answers/Apex Answers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apex Answers
+//
+//  Created by Ivan Maksimovic on 26/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!").padding()
+//    Text("High").position()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
